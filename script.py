61d909272065013f1a5708aa4f3eae74dd1cccd9
tiramirez <tiramirez@uc.cl>
2021-01-03 21:37:34 -0500

    set up

---
+++ script.py
@@ -0,0 +1,33 @@
+# %% 
+import pandas as pd
+import matplotlib.pyplot as plt 
+import matplotlib.dates as dt
+import numpy as np
+
+import geopandas as gpd
+
+
+# %% [markdown]
+
+https://stackoverflow.com/questions/54734667/error-installing-geopandas-a-gdal-api-version-must-be-specified-in-anaconda
+pip install descartes
+
+
+
+# %%
+A = np.random.random(10)
+
+# %%
+list = gpd.datasets
+# %%
+
+
+chile = gpd.read_file('Microdatos_Censo_2017%3A_Manzana-shp.zip')
+# %%
+cities = gpd.read_file(gpd.datasets.get_path('naturalearth_cities'))
+nybb = gpd.read_file(gpd.datasets.get_path('nybb'))
+
+# %%
+
+Chile last Census is 2017
+NYC las
